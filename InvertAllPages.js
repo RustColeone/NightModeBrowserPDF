41bc462b8c140f745d596e8d128785ba072e3440
RustColeone <leoyan@yahoo.com>
2020-01-17 20:10:12 -0800

    Better fit size of page

--- InvertAllPages.js
+++ InvertAllPages.js
@@ -1,7 +1,7 @@
 javascript:(function(){
     /*For Chrome, Firefox, and Safari*/
     var cover = document.createElement("InvertLayer");
-    cover.setAttribute("style", 'position: fixed;pointer-events: none;width: 100vw;height: 100vh;background-color: white;mix-blend-mode: difference;z-index: 1;', "id", "InvertLayer");
+    cover.setAttribute("style", 'position: fixed; top: 0;buttom: 0; pointer-events: none; width: 100vw; height: 100vh; background-color: white; mix-blend-mode: difference; z-index: 1;', "id", "InvertLayer");
     if(document.getElementById("InvertLayer")){
         document.getElementById("InvertLayer").remove();
     }
